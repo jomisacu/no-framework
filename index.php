99cea802d667a0cb874ba5d3dc7a3512eebfa08b
jomisacu <jomisacu.software@gmail.com>
2022-05-17 17:39:12 -0400

    restore to prev state

--- index.php
+++ index.php
@@ -1,16 +1,3 @@
 <?php
 
-use Jomisacu\NoFramework\Apps\Api\RegisterLocationController;
-
 require __DIR__ . '/bootstrap.php';
-
-$commands = [
-    'register_location' => RegisterLocationController::class,
-];
-
-function request_filterVar(string $variableName, $default = null) {
-    return $_REQUEST[$variableName] ?? $default;
-}
-
-
-// $_REQUEST, $_POST, $_GET, $_FILES
